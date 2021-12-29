ffdd65b8fe73268362d9bee96eb3bcd207a99e0f
Rajagopal Sreenivasan <rajagopal86@gmail.com>
2021-12-29 15:00:09 +0530

    more mmaps

---
+++ _notes/SRTS/3TatvaTrayam/mindmaps/saugata darshana.mm
@@ -0,0 +1,532 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="saugata&#xa;darshana" FOLDED="false" ID="ID_64074215" CREATED="1481107088138" MODIFIED="1481107122005" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.1">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Goal" POSITION="right" ID="ID_25330391" CREATED="1481107124361" MODIFIED="1481107127403">
+<edge COLOR="#ff0000"/>
+<node TEXT="nirvaana = duhkha nivritti" ID="ID_551891717" CREATED="1481107131209" MODIFIED="1481107177292"/>
+</node>
+<node TEXT="Basic tenets" POSITION="left" ID="ID_1172906086" CREATED="1481107188103" MODIFIED="1481107682653">
+<edge COLOR="#0000ff"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      bhaavana
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="Atma bhaava" ID="ID_1755554999" CREATED="1481107202174" MODIFIED="1481107480432">
+<node TEXT="moha" ID="ID_1617044223" CREATED="1481107351856" MODIFIED="1481107585547">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_551891717" MIDDLE_LABEL="cessation leads to" STARTINCLINATION="6;-24;" ENDINCLINATION="-142;-257;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="aham" ID="ID_81069690" CREATED="1481107268630" MODIFIED="1481107369203"/>
+<node TEXT="mama" ID="ID_1914249287" CREATED="1481107271797" MODIFIED="1481107365210"/>
+</node>
+</node>
+<node TEXT="jagat" ID="ID_1140863621" CREATED="1481107227820" MODIFIED="1481108258534">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1392344959" MIDDLE_LABEL="naashayati (destroys)" STARTINCLINATION="-4;94;" ENDINCLINATION="-4;-72;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="kShaNikam" ID="ID_689644880" CREATED="1481107234357" MODIFIED="1481107241034"/>
+<node TEXT="shUnyam" ID="ID_1002029796" CREATED="1481107377487" MODIFIED="1481107387440"/>
+</node>
+<node TEXT="duhkham" ID="ID_1842974661" CREATED="1481107251158" MODIFIED="1481107256088">
+<node TEXT="Cause" ID="ID_1484188432" CREATED="1481107723572" MODIFIED="1481107726208">
+<node TEXT="bandha / bhrama" ID="ID_1392344959" CREATED="1481107727800" MODIFIED="1481108541683">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1107931660" MIDDLE_LABEL="caused by" STARTINCLINATION="31;41;" ENDINCLINATION="40;-11;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="satyatvam" ID="ID_1658756166" CREATED="1481107747815" MODIFIED="1481107765817"/>
+<node TEXT="sthiratvam" ID="ID_760259083" CREATED="1481107766207" MODIFIED="1481107770435"/>
+<node TEXT="anukUlatvam" ID="ID_1927070931" CREATED="1481107927776" MODIFIED="1481107990755"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      feeling of favorability
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="anugatatvam" ID="ID_945703805" CREATED="1481107933314" MODIFIED="1481108150031"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      common
+    </p>
+    <p>
+      characteristics of a class
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="svalakshanam" ID="ID_916555935" CREATED="1481107257507" MODIFIED="1481107262187"/>
+</node>
+</node>
+<node TEXT="vaasana" POSITION="left" ID="ID_652874203" CREATED="1481107217103" MODIFIED="1481107694837">
+<edge COLOR="#00ff00"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      pos
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="definition" ID="ID_576704741" CREATED="1481108272406" MODIFIED="1481108284366">
+<node TEXT="kShaNAnAm santAnaH" ID="ID_1107931660" CREATED="1481108286176" MODIFIED="1481108332106"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      train of moments
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="santaana shakti" ID="ID_545834925" CREATED="1481108372167" MODIFIED="1481108385007"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      binding force
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="sajAtIya" ID="ID_1056588386" CREATED="1481108400186" MODIFIED="1481108412416"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      similar
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="kramotpanna" ID="ID_1416631470" CREATED="1481108403899" MODIFIED="1481108424304"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      sequential
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="pratyaasanna" ID="ID_400485247" CREATED="1481108427455" MODIFIED="1481108444911"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      in close proximity
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Subdivisions" POSITION="right" ID="ID_1298089117" CREATED="1481108552323" MODIFIED="1481108559738">
+<edge COLOR="#ff00ff"/>
+<node TEXT="mAdhyamika / mahaayaanika" ID="ID_514663752" CREATED="1481108561407" MODIFIED="1481108595430"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      shuunya vaadi
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="mukti = arising of uccheda of" ID="ID_284571827" CREATED="1481108606584" MODIFIED="1481108641092">
+<node TEXT="rAgAdi jnaana santaana" ID="ID_169493720" CREATED="1481108643354" MODIFIED="1481108652651"/>
+</node>
+<node TEXT="deshanaa =" ID="ID_242509815" CREATED="1481108709218" MODIFIED="1481108724553"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      instructions
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="categories" ID="ID_1211644357" CREATED="1481108717080" MODIFIED="1481108849197">
+<node TEXT="sattva" ID="ID_1153923998" CREATED="1481108744412" MODIFIED="1481108887729"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      jaati
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="aashaya" ID="ID_184633908" CREATED="1481108854189" MODIFIED="1481108894468"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      intention
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="vasha" ID="ID_153496142" CREATED="1481108856747" MODIFIED="1481108901863"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      dependence
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="upaaya" ID="ID_269207780" CREATED="1481108862582" MODIFIED="1481108916776"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      means or saadhana
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="intensity" ID="ID_912385200" CREATED="1481108807685" MODIFIED="1481108810852">
+<node TEXT="gambhiira" ID="ID_1024152145" CREATED="1481108812022" MODIFIED="1481108929194"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      deep
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="uttAna" ID="ID_453109166" CREATED="1481108817763" MODIFIED="1481108936089"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      surfacial
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="ubhaya" ID="ID_491513662" CREATED="1481108820758" MODIFIED="1481108941050"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      both
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="postulates two types of satya" ID="ID_335530324" CREATED="1481109013918" MODIFIED="1481109086538"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      reality
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="samvriti" ID="ID_43689958" CREATED="1481109018270" MODIFIED="1481109072638"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      apparent
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="paramaartha" ID="ID_1651527148" CREATED="1481109042779" MODIFIED="1481109079097"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      actual
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="shUnyataa = objects don&apos;t exist" ID="ID_149472145" CREATED="1481108953202" MODIFIED="1481109519868"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      knowledge is objectless
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="advaya lakShaNa" ID="ID_1612076816" CREATED="1481108959864" MODIFIED="1481108985822"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      opposed to multiplicity
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="vaibhaaShika" ID="ID_1737492284" CREATED="1481109097879" MODIFIED="1481109161730">
+<node TEXT="artha is jnaanaanvita" ID="ID_861653459" CREATED="1481109163836" MODIFIED="1481109203982"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      knowledge projects the object
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="sautraantika" ID="ID_389853800" CREATED="1481109208304" MODIFIED="1481109211767">
+<node TEXT="objects not external but perceived" ID="ID_1226324410" CREATED="1481109212984" MODIFIED="1481109268223"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      knower chooses to perceive
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="yogaachaara" ID="ID_872024221" CREATED="1481109272404" MODIFIED="1481109275196">
+<node TEXT="aakaara sahitaa buddhiH" ID="ID_769203306" CREATED="1481109276912" MODIFIED="1481109317862"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      object can also be discriminated thru its aakaara or form
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node TEXT="means of knowledge" POSITION="right" ID="ID_1287615182" CREATED="1481109420339" MODIFIED="1481109432335">
+<edge COLOR="#00ffff"/>
+<node TEXT="pratyaksha" ID="ID_469885068" CREATED="1481109433522" MODIFIED="1481109450448"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      sensory perception
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="anumaana" ID="ID_1825135827" CREATED="1481109438811" MODIFIED="1481109456829"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      inference
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
